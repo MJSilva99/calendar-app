c2663d8a130ffc18b88637f6d04324613175ed8a
Michael <michaelj.silva@hotmail.com>
2023-12-12 21:09:24 +0000

    For loop to create time blocks, created new elements for html and appended them

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function displayCurrentDay() {
     $("#currentDay").text(currentDay);
 }
 displayCurrentDay();
+
+// Loop to create time blocks for each hour from 9 to 17
+for (var currentHour = 9; currentHour <= 17; currentHour++) {
+    createTimeBlock(currentHour);
+}
+
+// Function to create a time block for a specific hour
+function createTimeBlock(currentHour) {
+    // Create a new div element with the 'time-block' class and a unique id
+    var timeBlock = $("<div>").addClass("row time-block").attr("id", "hour-" + currentHour);
+    
+    // Get the formatted hour using dayjs
+    var hour = dayjs().hour(currentHour).format("hA");
+    
+    // Create a textarea element with the 'description' class and a unique id
+    var textArea = $("<textarea>").addClass("col-md-10 description").attr("id", "hour-" + currentHour);
+    
+    // Created a button element with the 'saveBtn' class and an icon
+    var saveBtn = $("<button>").addClass("col-md-1 saveBtn").append($("<i>").addClass("fas fa-save"));
+    
+    // Created a div element to display the hour
+    var hourBlock = $("<div>").addClass("col-md-1 hour").text(hour);
+
+    // Append the hour, textarea, and button to the time block
+    timeBlock.append(hourBlock);
+    timeBlock.append(textArea);
+    timeBlock.append(saveBtn);
+
+    $(".container").append(timeBlock);
+
+
+}
